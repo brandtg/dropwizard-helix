4efc359df5e7f39886191486b24ba8e3662f5926
Greg Brandt <brandt.greg@gmail.com>
2015-12-05 15:34:30 -0800

    Adding license

--- src/test/java/com/github/brandtg/discovery/DropWizardApplicationRunner.java
+++ src/test/java/com/github/brandtg/discovery/DropWizardApplicationRunner.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright (C) 2015 Greg Brandt (brandt.greg@gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.github.brandtg.discovery;
 
 import com.codahale.metrics.MetricRegistry;
